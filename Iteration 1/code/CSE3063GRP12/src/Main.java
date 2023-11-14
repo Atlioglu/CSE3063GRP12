b844ec95d495e57917f9536618fbd909b7f5f5ac
Ömer Geysoğlu <ogeysoglu@outlook.com>
2023-11-14 22:03:55 +0300

    Feauture: added Login #63
    closes #63

--- Iteration 1/code/CSE3063GRP12/src/Main.java
+++ Iteration 1/code/CSE3063GRP12/src/Main.java
@@ -1,5 +1,7 @@
+import core.login.LoginController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new LoginController();
     }
 }
