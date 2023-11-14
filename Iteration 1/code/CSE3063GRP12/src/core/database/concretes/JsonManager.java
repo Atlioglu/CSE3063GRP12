1ef27fbd0cbc990a054a079a48a1d99afe50b202
furkan acar <65075121+AcarFurkan@users.noreply.github.com>
2023-11-14 18:52:35 +0300

    Refactor: test

--- Iteration 1/code/CSE3063GRP12/src/core/database/concretes/JsonManager.java
+++ Iteration 1/code/CSE3063GRP12/src/core/database/concretes/JsonManager.java
@@ -7,6 +7,7 @@ import core.database.abstracts.DatabaseManager;
 
 public class JsonManager extends DatabaseManager {
     public Map<String,Object> read(String key){
+        
         return null;
     } 
     public void write(String key, Map<String,Object> map){
