0950b5689024913f7a259fdfb71fb9f0ef739bd3
Ardacan ÖZENER <43501512+DrHannibalLecter@users.noreply.github.com>
2023-11-17 15:01:51 +0200

    Add files via upload

---
+++ Iteration 1/code/CSE3063GRP12/src/features/CourseApprovalController.java
@@ -0,0 +1,65 @@
+package features.Advisor;
+
+import core.general_providers.TerminalManager;
+import core.repositories.CourseEnrollmentRepository;
+import core.models.concretes.CourseEnrollment;
+import core.exceptions.UnexpectedInputException;
+import features.approval_courses_selected.ApprovalCoursesSelectedController;
+import features.main_menu.MenuController;
+import java.util.ArrayList;
+
+public class CourseApprovalController {
+    private CourseApprovalView courseApprovalView;
+    private CourseEnrollmentRepository courseEnrollmentRepository;
+
+    public CourseApprovalController() {
+        courseEnrollmentRepository = new CourseEnrollmentRepository();
+        courseApprovalView = new CourseApprovalView();
+
+        try {
+            handleApprovalController();
+        } catch (UnexpectedInputException e) {
+            courseApprovalView.showErrorMessage(e);
+        }
+    }
+
+    private ArrayList<CourseEnrollment> fetchPendingEnrollments() {
+        return courseEnrollmentRepository.getPendingEnrollments();
+
+    }
+
+    private void navigateToApprovalCoursesSelected(CourseEnrollment courseEnrollment) {
+
+        new ApprovalCoursesSelectedController(courseEnrollment);
+    }
+
+    private void navigateToMenu() {
+        new MenuController();
+    }
+
+    private String getUserInput() {
+        String input = TerminalManager.getInstance().read();
+        // TerminalManager.getInstance().dispose();
+        return input;
+    }
+
+    private void handleApprovalController() throws UnexpectedInputException {
+        ArrayList<CourseEnrollment> pendingEnrollments = fetchPendingEnrollments();
+        courseApprovalView.showPendingCourseEnrollments(pendingEnrollments);
+
+        courseApprovalView.showPromptMessage();
+        String selection = getUserInput();
+        if (selection.matches("^\\d+$") == false && selection.equals("q") == false) {
+            throw new UnexpectedInputException();
+        } else if (selection.equals("q")) {
+            navigateToMenu();
+        } else {
+            int index = Integer.parseInt(selection);
+            if (index > courseEnrollmentRepository.getPendingEnrollments().size() || index < 0) {
+                throw new UnexpectedInputException();
+            } else {
+                navigateToApprovalCoursesSelected(courseEnrollmentRepository.getPendingEnrollments().get(index - 1));
+            }
+        }
+    }
+}
