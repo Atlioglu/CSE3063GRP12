e53a2f67d568f13f5ac4fca39e4d1b0027811891
furkan acar <65075121+AcarFurkan@users.noreply.github.com>
2023-11-17 07:30:31 +0300

    Refactor: updating errors and adding jsons

--- Iteration 1/code/CSE3063GRP12/src/features/Advisor/AdvisorStudentListController.java
+++ Iteration 1/code/CSE3063GRP12/src/features/Advisor/AdvisorStudentListController.java
@@ -3,39 +3,39 @@ package features.Advisor;
 import core.general_providers.TerminalManager;
 import core.general_providers.SessionController;
 import core.models.concretes.Advisor;
+import core.repositories.UserRepository;
 import features.main_menu.MenuController;
-
 
 public class AdvisorStudentListController {
     private AdvisorStudentListView advisorStudentListView;
+    private UserRepository userRepository;
 
-    public AdvisorStudentListController(){
-        advisorStudentListView=new AdvisorStudentListView();
+    public AdvisorStudentListController() {
+        advisorStudentListView = new AdvisorStudentListView();
 
         handleStudentList();
     }
 
-
-    private void navigateToMenu(){
+    private void navigateToMenu() {
         new MenuController();
     }
 
-    private String getUserInput(){
+    private String getUserInput() {
         String input = TerminalManager.getInstance().read();
         TerminalManager.getInstance().dispose();
         return input;
     }
 
-    private void handleStudentList(){
-        while(true){
+    private void handleStudentList() {
+        while (true) {
             System.out.println("Type \"q\" to see the menu\n Type \"Show\" to see the student list");
             String input = getUserInput();
-            if(input.equals("q")){
+            if (input.equals("q")) {
                 navigateToMenu();
                 break;
             } else if (input.equals("Show")) {
                 Advisor a = (Advisor) SessionController.getInstance().getCurrentUser();
-                advisorStudentListView.showStudentList(a.getListOfStudents());
+                advisorStudentListView.showStudentList(userRepository.getStudentsByAdvisor(a));
             }
         }
     }
--- Iteration 1/code/CSE3063GRP12/src/features/Advisor/CourseApprovalController.java
+++ Iteration 1/code/CSE3063GRP12/src/features/Advisor/CourseApprovalController.java
@@ -11,58 +11,54 @@ public class CourseApprovalController {
     private CourseApprovalView courseApprovalView;
     private CourseEnrollmentRepository courseEnrollmentRepository;
 
-    public CourseApprovalController(){
+    public CourseApprovalController() {
         courseEnrollmentRepository = new CourseEnrollmentRepository();
         courseApprovalView = new CourseApprovalView();
 
         handleApprovalController();
     }
 
-
     private ArrayList<CourseEnrollment> fetchPendingEnrollments() {
         return courseEnrollmentRepository.getPendingEnrollments();
     }
 
+    // TODO: Handle unexpected input
     private int getUserSelection() {
         System.out.println("Type \"q\" to see the menu\nType Student Id to process course enrollment");
         ArrayList<CourseEnrollment> courseEnrollmentList = fetchPendingEnrollments();
         courseApprovalView.showPendingCourseEnrollments(courseEnrollmentList);
         String input = TerminalManager.getInstance().read();
         TerminalManager.getInstance().dispose();
-        try{
-            if(input.equals("q")){
+        try {
+            if (input.equals("q")) {
                 return -1;
-            }
-            else{
-                for(int i = 0;i < courseEnrollmentList.size();i++){
+            } else {
+                for (int i = 0; i < courseEnrollmentList.size(); i++) {
                     courseEnrollmentList.get(i).getStudentId().equals(input);
                     return i;
                 }
             }
-            new UnexpectedInputException();
-        }
-        catch (UnexpectedInputException e){
-            new UnexpectedInputException();
+            throw new UnexpectedInputException();
+        } catch (UnexpectedInputException e) {
             return -2;
         }
     }
 
-    private void navigateToApprovalCoursesSelected(CourseEnrollment courseEnrollment){
-        new ApprovalCoursesSelected(courseEnrollment);
+    private void navigateToApprovalCoursesSelected(CourseEnrollment courseEnrollment) {
+      //  new ApprovalCoursesSelected(courseEnrollment);
     }
 
-    private void navigateToMenu(){
+    private void navigateToMenu() {
         new MenuController();
     }
 
-    private void handleApprovalController(){
-        while(true) {
+    private void handleApprovalController() {
+        while (true) {
             int selection = getUserSelection();
-            if(selection == -1){
+            if (selection == -1) {
                 navigateToMenu();
                 break;
-            }
-            else if(selection>=0){
+            } else if (selection >= 0) {
                 navigateToApprovalCoursesSelected(fetchPendingEnrollments().get(selection));
                 break;
             }
