1ab96cb7c1f9190c16d3ba8ff1ace8e1534607fa
furkan acar <65075121+AcarFurkan@users.noreply.github.com>
2023-12-06 13:32:36 +0300

    Refactor: updating semester info

--- Iteration 1/code/CSE3063GRP12/src/core/repositories/TranscriptRepository.java
+++ Iteration 1/code/CSE3063GRP12/src/core/repositories/TranscriptRepository.java
@@ -4,18 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import core.database.abstracts.DatabaseManager;
 import core.enums.CourseGrade;
 import core.exceptions.UserNotFoundException;
 import core.general_providers.AppConstant;
 import core.general_providers.InstanceManager;
-import core.general_providers.SessionController;
 import core.models.abstracts.User;
 import core.models.concretes.Transcript;
 import core.models.concretes.Course;
@@ -42,15 +35,17 @@ public class TranscriptRepository {
         return databaseManager.read(path + "/" + currentSemester + "/" + studentId + ".json", Transcript.class);
     }
 
-    public void createTranscript(Transcript transcript) throws IOException {
+    // public void createTranscript(Transcript transcript) throws IOException {
 
-        databaseManager.write(path + "/" + SessionController.getInstance().getCurrentUser().getUserName() + ".json",
-                transcript);
-    }
+    // databaseManager.write(path + "/" +
+    // SessionController.getInstance().getCurrentUser().getUserName() + ".json",
+    // transcript);
+    // }
 
     // TODO: WHY WE NEED THIS?
     public ArrayList<Course> getCoursesForSemesterByStudent(User user) throws IOException {
-        Transcript transcript = databaseManager.read(path + "/" + user.getUserName()
+        int currentSemester = ((Student) user).getTranscript().getCurrentSemester();
+        Transcript transcript = databaseManager.read(path + "/" + currentSemester + "/" + user.getUserName()
                 + ".json", Transcript.class);
 
         ArrayList<Semester> semesters = transcript.getListOfSemester();
@@ -61,23 +56,24 @@ public class TranscriptRepository {
         return new ArrayList<>();
     }
 
-    public List<String> findFilesWithId(String directoryPath, String id) {
-        String pa = System.getProperty("user.dir") + AppConstant.getInstance().getBasePath() + "/course/";
-        List<String> matchedFiles = new ArrayList<>();
-        Path startPath = Paths.get(pa);
+    // public List<String> findFilesWithId(String directoryPath, String id) {
+    // String pa = System.getProperty("user.dir") +
+    // AppConstant.getInstance().getBasePath() + "/course/";
+    // List<String> matchedFiles = new ArrayList<>();
+    // Path startPath = Paths.get(pa);
 
-        try {
-            Files.walk(startPath)
-                    .filter(Files::isRegularFile)
-                    .filter(path -> path.toString().endsWith(".json"))
-                    .filter(path -> path.getFileName().toString().contains(id))
-                    .forEach(path -> matchedFiles.add(path.toString()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    // try {
+    // Files.walk(startPath)
+    // .filter(Files::isRegularFile)
+    // .filter(path -> path.toString().endsWith(".json"))
+    // .filter(path -> path.getFileName().toString().contains(id))
+    // .forEach(path -> matchedFiles.add(path.toString()));
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
 
-        return matchedFiles;
-    }
+    // return matchedFiles;
+    // }
 
     public ArrayList<Course> _fetchCourseListByStudentListId(ArrayList<String> id) {
 
@@ -99,9 +95,9 @@ public class TranscriptRepository {
             }
             int totalCreditTaken = courseEnrollment.getSelectedCourseList().stream().mapToInt(Course::getCredit).sum();
 
-            Semester semester = new Semester("0", newCourseList, totalCreditTaken, 0);
+            Semester semester = new Semester("0", newCourseList, totalCreditTaken, 0, transcript.getCurrentSemester());
             transcript.getListOfSemester().add(semester);
-            databaseManager.write(path + "/" + courseEnrollment.getStudentId() + ".json",
+            databaseManager.write(path + "/" + currentSemester + "/" + courseEnrollment.getStudentId() + ".json",
                     transcript);
         } else {
             // update current semester
@@ -113,7 +109,7 @@ public class TranscriptRepository {
             int totalCreditTaken = courseEnrollment.getSelectedCourseList().stream().mapToInt(Course::getCredit).sum();
 
             Semester semester = new Semester("0", newCourseList, totalCreditTaken,
-                    0);
+                    0, transcript.getCurrentSemester());
             transcript.getListOfSemester().set(currentSemester - 1, semester);
             databaseManager.write(path + "/" + courseEnrollment.getStudentId() + ".json",
                     transcript);
