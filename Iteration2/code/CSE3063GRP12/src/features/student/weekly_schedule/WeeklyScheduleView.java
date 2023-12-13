1a966aefadd1847c7ee452d4fba160d703c0aa3d
Pirate-960 <a.a.zahran.960@gmail.com>
2023-12-14 01:55:23 +0300

    -- Better Week Schedule View --
    -- Better Week Schedule View --

--- Iteration2/code/CSE3063GRP12/src/features/student/weekly_schedule/WeeklyScheduleView.java
+++ Iteration2/code/CSE3063GRP12/src/features/student/weekly_schedule/WeeklyScheduleView.java
@@ -9,19 +9,24 @@ import core.models.concretes.Course;
 
 public class WeeklyScheduleView {
     public void showWeeklySchedule(ArrayList<Course> courses) {
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+        System.out.printf("\t| %-60s | %-15s | %-15s |\n", "Course Name", "Course Day", "Course Slot");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+    
         for (Course course : courses) {
-            for (Map.Entry<CourseDay, ArrayList<CourseSlot>> entry : course.getSession().getCourseSessions()
-                    .entrySet()) {
-                System.out.println("Course Name: " + course.getName());
-                System.out.println("Course Day: " + entry.getKey());
+            for (Map.Entry<CourseDay, ArrayList<CourseSlot>> entry : course.getSession().getCourseSessions().entrySet()) {
+                String courseName = course.getName();
+                CourseDay courseDay = entry.getKey();
+    
                 for (CourseSlot slot : entry.getValue()) {
-                    System.out.println("Course Slot: " + slot.getSlot());
+                    String courseSlot = slot.getSlot();
+                    System.out.printf("\t| %-60s | %-15s | %-15s |\n", courseName, courseDay, courseSlot);
                 }
             }
-            System.out.println();
+            System.out.println("--------------------------------------------------------------------------------------------------------------------");
         }
-
     }
+    
 
     public void showErrorMessage(Exception e) {
         System.out.println("Error: " + e.getMessage());
