acc277a39cc18ed0d56eee70d320130463075395
Pirate-960 <a.a.zahran.960@gmail.com>
2023-12-15 21:52:52 +0300

    -- Better Student List View --
    -- Better Student List View --

--- Iteration2/code/CSE3063GRP12/src/features/advisor/student_list/AdvisorStudentListView.java
+++ Iteration2/code/CSE3063GRP12/src/features/advisor/student_list/AdvisorStudentListView.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 
 public class AdvisorStudentListView {
     public void showStudentList(ArrayList<Student> studentArrayList){
+        System.out.println("======================================================================================Student List======================================================================================");
+        System.out.printf("%-27s%-20s%-20s\n", "[Student ID]", "[First Name]", "[Last Name]");
+        System.out.println("========================================================================================================================================================================================");
         for(int i = 1;i<=studentArrayList.size();i++){
-            System.out.println(i + ". " + studentArrayList.get(i-1).getUserName() + " " + studentArrayList.get(i-1).getLastName());
+            System.out.printf("[%-2d]. %-20s %-20s %-20s\n", i, studentArrayList.get(i - 1).getUserName(), studentArrayList.get(i - 1).getFirstName(), studentArrayList.get(i - 1).getLastName());
         }
+        System.out.println("========================================================================================================================================================================================");
     }
     public void showQuitMessage(){
         System.out.print("Press q to return Main Menu: ");
