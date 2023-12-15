8ee968a2743c87bbd829cfc6cca59863cdb48a9e
Abdelrahman Zahran <101503244+Abdelrahman-Zahran-960@users.noreply.github.com>
2023-12-15 23:13:13 +0300

    Update AdvisorStudentListView.java

--- Iteration2/code/CSE3063GRP12/src/features/advisor/student_list/AdvisorStudentListView.java
+++ Iteration2/code/CSE3063GRP12/src/features/advisor/student_list/AdvisorStudentListView.java
@@ -9,7 +9,7 @@ public class AdvisorStudentListView {
         System.out.printf("%-27s%-20s%-20s\n", "[Student ID]", "[First Name]", "[Last Name]");
         System.out.println("========================================================================================================================================================================================");
         for(int i = 1;i<=studentArrayList.size();i++){
-            System.out.printf("[%-2d]. %-20s %-20s %-20s\n", i, studentArrayList.get(i - 1).getUserName(), studentArrayList.get(i - 1).getFirstName(), studentArrayList.get(i - 1).getLastName());
+            System.out.printf("[%-2d] %-20s %-20s %-20s\n", i, studentArrayList.get(i - 1).getUserName(), studentArrayList.get(i - 1).getFirstName(), studentArrayList.get(i - 1).getLastName());
         }
         System.out.println("========================================================================================================================================================================================");
     }
