170f29e390007b18b88348b618a25400e4461f8f
DrHannibalLecter <ardacanozener@gmail.com>
2023-12-19 18:36:36 +0300

    Issue in CourseApproval and AdvisorStudentList Classes is solved

--- Iteration2/code/CSE3063GRP12/src/features/advisor/student_list/AdvisorStudentListView.java
+++ Iteration2/code/CSE3063GRP12/src/features/advisor/student_list/AdvisorStudentListView.java
@@ -1,9 +1,10 @@
 package features.advisor.student_list;
 
+import core.exceptions.UnexpectedInputException;
 import core.models.concretes.Student;
 import java.util.ArrayList;
 
-public class AdvisorStudentListView {
+public class AdvisorStudentListView{
     public void showStudentList(ArrayList<Student> studentArrayList){
         for(int i = 1;i<=studentArrayList.size();i++){
             System.out.println(i + ". " + studentArrayList.get(i-1).getUserName() + " " + studentArrayList.get(i-1).getLastName());
@@ -11,6 +12,9 @@ public class AdvisorStudentListView {
     }
     public void showQuitMessage(){
         System.out.print("Press q to return Main Menu: ");
+    }
+    public void showErrorMessage() throws UnexpectedInputException{
+        throw new UnexpectedInputException();
     }
 
 }
