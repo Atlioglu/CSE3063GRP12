15bbe75e36a4c246e000e0f078e9084e3af2de6e
Ömer Geysoğlu <ogeysoglu@outlook.com>
2024-01-03 21:53:22 +0300

    Refactor: CourseEnrollment model

--- Iteration3/code/CSE3063GRP12/src/core/models/concretes/CourseEnrollment.py
+++ Iteration3/code/CSE3063GRP12/src/core/models/concretes/CourseEnrollment.py
@@ -1,9 +1,9 @@
 class CourseEnrollment:
-    def __init__(self, id, selectedCourseList, student_id, approvalState):
+    def __init__(self, id, selectedCourseList, studentId, approvalState):
         self.id = id
         self.selectedCourseList = selectedCourseList  # list of Course instances
         self.approved_course_list = []  
         self.rejected_course_list = [] 
-        self.student_id = student_id
+        self.studentId = studentId
         self.approvalState = approvalState  # instance of ApprovalState enum
 
